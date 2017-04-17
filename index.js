74761f711b365c06f855c5533f6418de6e0dc5b5
ianeflint <ian.flint@icloud.com>
2017-04-17 17:49:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,155 @@
+var express = require('express')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var request = require('request')
+var transitDestinations = require('./public/transitDestinations')
+var fs = require('fs')
+var JSONStream = require('JSONStream')
+var es = require('event-stream')
+
+
+// used to reduce NYC Pluto data into a smaller file by only saving important stuff
+var convertPluto = function (inputURL, district) {
+	var sampleFC = {
+		type: 'FeatureCollection',
+		features: []
+	}
+	fs.createReadStream(inputURL)
+	.pipe(JSONStream.parse('features.*'))
+	.on('data', function(element) {
+		var featureObj = {
+			type: 'Feature',
+			geometry: element['geometry'],
+			properties: {
+				sqftVal: element['properties']['assesstot']/element['properties']['bldgarea'],
+				// borough: element['properties']['borough'],
+				cd: element['properties']['cd'],
+				// address: element['properties']['address'],
+				// zipcode: element['properties']['zipcode'],
+				// landuse: element['properties']['landuse'],
+				// ownertype: element['properties']['ownertype'],
+				// bldgclass: element['properties']['bldgclass'],
+				// policeprct: element['properties']['policeprct'],
+				// schooldist: element['properties']['schooldist'],
+				assesstot: element['properties']['assesstot'],
+				assessland: element['properties']['assessland'],
+				yearbuilt: element['properties']['yearbuilt'],
+				yearalter1: element['properties']['yearalter1'],
+				yearalter2: element['properties']['yearalter2'],
+				// numbldgs: element['properties']['numbldgs'],
+				numfloors: element['properties']['numfloors'],
+				height: element['properties']['numfloors'] * 3,
+				unitstotal: element['properties']['unitstotal'],
+				unitsres: element['properties']['unitsres'],
+				lotarea: element['properties']['lotarea'],
+				bldgarea: element['properties']['bldgarea'],
+				comarea: element['properties']['comarea'],
+				resarea: element['properties']['resarea'],
+				officearea: element['properties']['officearea'],
+				retailarea: element['properties']['retailarea'],
+				garagearea: element['properties']['garagearea'],
+				strgearea: element['properties']['strgearea'],
+				factryarea: element['properties']['factryarea'],
+				otherarea: element['properties']['otherarea']
+			}
+		}
+		if (element['properties']['cd'] == district) {
+			sampleFC.features.push(featureObj)
+		}
+	})
+	.on('end', function() {
+		fs.appendFile('pluto' + district + '.geojson', JSON.stringify(sampleFC))
+		console.log('done')
+	})
+}
+
+// use this to loop through all districts in a bourough
+
+// for (var i = 501; i < 504; i++) {
+// 	convertPluto('./public/ogPlutoData/simappluto.geojson', i)
+// 	console.log(i)
+// }
+
+
+// Router
+
+app.use(express.static('public'))
+server.listen(3000)
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/home.html')
+});
+
+app.get('/admin', function (req, res) {
+  res.sendFile(__dirname + '/admin.html')
+});
+
+app.get('/blog', function (req, res) {
+	res.sendFile(__dirname + '/blog.html')
+});
+
+app.get('/data', function (req, res) {
+	res.sendFile(__dirname + '/data.html')
+});
+
+app.get('/maps', function (req, res) {
+	res.sendFile(__dirname + '/mapMenu.html')
+});
+
+app.get('/maps/:district/:data', function (req, res) {
+	res.sendFile(__dirname + '/maps.html')
+});
+
+
+
+//	calculates average travel time to a group of locations in transitDestinations.js
+
+var googleApiKey = 'AIzaSyBAp19Y8cPv8TNzNfJ8OiUxaMHEGL_ELPY'
+
+var destinationsString = ''
+var dFeatures = transitDestinations.transitDestinations.features
+for (var i = 0; i < dFeatures.length; i++) {
+	if (i < dFeatures.length - 1) {
+		destinationsString = destinationsString + dFeatures[i].geometry.coordinates[1] + ',' + dFeatures[i].geometry.coordinates[0] + '|'
+	} else {
+		destinationsString = destinationsString + dFeatures[i].geometry.coordinates[1] + ',' + dFeatures[i].geometry.coordinates[0]
+	}
+}
+
+io.on('connection', function (socket) {
+  socket.on('searchAddress', function (data) {
+
+    var origins = data.address
+    request('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&mode=transit&origins=' + origins + '&destinations=' + destinationsString + '&key=AIzaSyBAp19Y8cPv8TNzNfJ8OiUxaMHEGL_ELPY', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    var responseObj = JSON.parse(body)
+	    var distancesArray = []
+	    var totalDuration = 0
+	    for (var i = 0; i < responseObj.destination_addresses.length; i++) {
+	    	var parseDuration = responseObj.rows[0].elements[i].duration.text
+	    	var parseDurationArray = parseDuration.split(' ')
+	    	var durationMinutes
+	    	if (parseDurationArray.length == 2) {
+	    		durationMinutes = parseInt(parseDurationArray[0])
+	    	} else if (parseDurationArray.length == 4) {
+	    		durationMinutes = parseInt(parseDurationArray[0]) * 60 + parseInt(parseDurationArray[2])
+	    	} else if (parseDurationArray.length == 6) {
+	    		durationMinutes = parseInt(parseDurationArray[0]) * 1440 + parseInt(parseDurationArray[2]) * 60 + parseInt(parseDurationArray[4])
+	    	}
+	    	var distanceObj = {
+	    		origin: responseObj.origin_addresses[0],
+	    		destination: responseObj.destination_addresses[i],
+	    		destinationName: transitDestinations.transitDestinations.features[i].properties.name,
+	    		mode: 'transit',
+	    		duration: durationMinutes
+	    	}
+	    	totalDuration = totalDuration + durationMinutes
+	    	distancesArray.push(distanceObj)
+	    }
+
+	    console.log(distancesArray, totalDuration/dFeatures.length)
+	  }
+	})
+  });
+});
---
+++ public/districts.js
@@ -0,0 +1,356 @@
+districts = {
+	101: {
+		borough: 'Manhattan',
+		district: 1,
+		center: [-74.008098, 40.711572],
+		zoom: 14
+	},
+	102: {
+		borough: 'Manhattan',
+		district: 2,
+		center: [-74.002776, 40.728160],
+		zoom: 14
+	},
+	103: {
+		borough: 'Manhattan',
+		district: 3,
+		center: [-73.986211, 40.720094],
+		zoom: 14
+	},
+	104: {
+		borough: 'Manhattan',
+		district: 4,
+		center: [-73.999214, 40.754595],
+		zoom: 14
+	},
+	105: {
+		borough: 'Manhattan',
+		district: 5,
+		center: [-73.986683, 40.748743],
+		zoom: 14
+	},
+	106: {
+		borough: 'Manhattan',
+		district: 6,
+		center: [-73.975525, 40.741427],
+		zoom: 14
+	},
+	107: {
+		borough: 'Manhattan',
+		district: 7,
+		center: [-73.977885, 40.783782],
+		zoom: 14
+	},
+	108: {
+		borough: 'Manhattan',
+		district: 8,
+		center: [-73.956985, 40.770750],
+		zoom: 14
+	},
+	109: {
+		borough: 'Manhattan',
+		district: 9,
+		center: [-73.960419, 40.812403],
+		zoom: 14
+	},
+	110: {
+		borough: 'Manhattan',
+		district: 10,
+		center: [-73.946214, 40.812046],
+		zoom: 14
+	},
+	111: {
+		borough: 'Manhattan',
+		district: 11,
+		center: [-73.939347, 40.796713],
+		zoom: 14
+	},
+	112: {
+		borough: 'Manhattan',
+		district: 12,
+		center: [-73.937030, 40.848414],
+		zoom: 14
+	},
+	201: {
+		borough: 'Bronx',
+		district: 1,
+		center: [-74.008098, 40.711572],
+		zoom: 14
+	},
+	202: {
+		borough: 'Bronx',
+		district: 2,
+		center: [-74.002776, 40.728160],
+		zoom: 14
+	},
+	203: {
+		borough: 'Bronx',
+		district: 3,
+		center: [-73.986211, 40.720094],
+		zoom: 14
+	},
+	204: {
+		borough: 'Bronx',
+		district: 4,
+		center: [-73.999214, 40.754595],
+		zoom: 14
+	},
+	205: {
+		borough: 'Bronx',
+		district: 5,
+		center: [-73.986683, 40.748743],
+		zoom: 14
+	},
+	206: {
+		borough: 'Bronx',
+		district: 6,
+		center: [-73.975525, 40.741427],
+		zoom: 14
+	},
+	207: {
+		borough: 'Bronx',
+		district: 7,
+		center: [-73.977885, 40.783782],
+		zoom: 14
+	},
+	208: {
+		borough: 'Bronx',
+		district: 8,
+		center: [-73.956985, 40.770750],
+		zoom: 14
+	},
+	209: {
+		borough: 'Bronx',
+		district: 9,
+		center: [-73.960419, 40.812403],
+		zoom: 14
+	},
+	210: {
+		borough: 'Bronx',
+		district: 10,
+		center: [-73.946214, 40.812046],
+		zoom: 14
+	},
+	211: {
+		borough: 'Bronx',
+		district: 11,
+		center: [-73.939347, 40.796713],
+		zoom: 14
+	},
+	212: {
+		borough: 'Bronx',
+		district: 12,
+		center: [-73.937030, 40.848414],
+		zoom: 14
+	},
+	301: {
+		borough: 'Brooklyn',
+		district: 1,
+		center: [-74.008098, 40.711572],
+		zoom: 13
+	},
+	302: {
+		borough: 'Brooklyn',
+		district: 2,
+		center: [-73.930513, 40.737102],
+		zoom: 13
+	},
+	303: {
+		borough: 'Brooklyn',
+		district: 3,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	304: {
+		borough: 'Brooklyn',
+		district: 4,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	305: {
+		borough: 'Brooklyn',
+		district: 5,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	306: {
+		borough: 'Brooklyn',
+		district: 6,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	307: {
+		borough: 'Brooklyn',
+		district: 7,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	308: {
+		borough: 'Brooklyn',
+		district: 8,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	309: {
+		borough: 'Brooklyn',
+		district: 9,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	310: {
+		borough: 'Brooklyn',
+		district: 10,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	311: {
+		borough: 'Brooklyn',
+		district: 11,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	312: {
+		borough: 'Brooklyn',
+		district: 12,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	313: {
+		borough: 'Brooklyn',
+		district: 13,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	314: {
+		borough: 'Brooklyn',
+		district: 14,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	315: {
+		borough: 'Brooklyn',
+		district: 11,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	316: {
+		borough: 'Brooklyn',
+		district: 12,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	317: {
+		borough: 'Brooklyn',
+		district: 13,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	318: {
+		borough: 'Brooklyn',
+		district: 14,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	401: {
+		borough: 'Queens',
+		district: 1,
+		center: [-74.008098, 40.711572],
+		zoom: 13
+	},
+	402: {
+		borough: 'Queens',
+		district: 2,
+		center: [-73.930513, 40.737102],
+		zoom: 13
+	},
+	403: {
+		borough: 'Queens',
+		district: 3,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	404: {
+		borough: 'Queens',
+		district: 4,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	405: {
+		borough: 'Queens',
+		district: 5,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	406: {
+		borough: 'Queens',
+		district: 6,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	407: {
+		borough: 'Queens',
+		district: 7,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	408: {
+		borough: 'Queens',
+		district: 8,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	409: {
+		borough: 'Queens',
+		district: 9,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	410: {
+		borough: 'Queens',
+		district: 10,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	411: {
+		borough: 'Queens',
+		district: 11,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	412: {
+		borough: 'Queens',
+		district: 12,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	413: {
+		borough: 'Queens',
+		district: 13,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	414: {
+		borough: 'Queens',
+		district: 14,
+		center: [-73.986211, 40.720094],
+		zoom: 13
+	},
+	501: {
+		borough: 'Staten Island',
+		district: 1,
+		center: [-74.008098, 40.711572],
+		zoom: 12
+	},
+	502: {
+		borough: 'Staten Island',
+		district: 2,
+		center: [-74.002776, 40.728160],
+		zoom: 12
+	},
+	503: {
+		borough: 'Staten Island',
+		district: 3,
+		center: [-73.986211, 40.720094],
+		zoom: 12
+	}
+}
