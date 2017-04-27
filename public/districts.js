5722a78cfa6f65953002602ad2a244f0ca66dc5c
ianeflint <ian.flint@icloud.com>
2017-04-27 14:47:03 -0400

    updated district names and centers

--- public/districts.js
+++ public/districts.js
@@ -192,283 +192,283 @@ districts = {
 		zoom: 14
 	},
 	301: {
-		title: '',
+		title: 'Williamsburg',
 		neighborhoods: 'Flushing Avenue, Williamsburg, Greenpoint, Northside, and Southside',
 		borough: 'Brooklyn',
 		district: 1,
-		center: [-74.008098, 40.711572],
+		center: [-73.948622, 40.717441],
 		zoom: 13
 	},
 	302: {
-		title: '',
-		neighborhoods: '	Brooklyn Heights, Fulton Mall, Boerum Hill, Fort Greene, Brooklyn Navy Yard, Fulton Ferry, and Clinton Hill',
+		title: 'Downtown Brooklyn',
+		neighborhoods: 'Brooklyn Heights, Fulton Mall, Boerum Hill, Fort Greene, Brooklyn Navy Yard, Fulton Ferry, and Clinton Hill',
 		borough: 'Brooklyn',
 		district: 2,
-		center: [-73.930513, 40.737102],
-		zoom: 13
+		center: [-73.981326, 40.692909],
+		zoom: 14
 	},
 	303: {
-		title: '',
+		title: 'Bedford-Stuyvesant',
 		neighborhoods: 'Bedford-Stuyvesant, Stuyvesant Heights, and Ocean Hill',
 		borough: 'Brooklyn',
 		district: 3,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.937713, 40.688055],
+		zoom: 14
 	},
 	304: {
-		title: '',
+		title: 'Bushwick',
 		neighborhoods: 'Bushwick',
 		borough: 'Brooklyn',
 		district: 4,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.913138, 40.693008],
+		zoom: 14
 	},
 	305: {
-		title: '',
+		title: 'East New York',
 		neighborhoods: 'East New York, Cypress Hills, Highland Park, New Lots, City Line, Starrett City, and Ridgewood',
 		borough: 'Brooklyn',
 		district: 5,
-		center: [-73.986211, 40.720094],
+		center: [-73.877462, 40.667324],
 		zoom: 13
 	},
 	306: {
-		title: '',
+		title: 'Park Slope',
 		neighborhoods: 'Red Hook, Carroll Gardens, Park Slope, Gowanus, and Cobble Hill',
 		borough: 'Brooklyn',
 		district: 6,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.992270, 40.677239],
+		zoom: 13.5
 	},
 	307: {
-		title: '',
+		title: 'Sunset Park',
 		neighborhoods: 'Sunset Park and Windsor Terrace',
 		borough: 'Brooklyn',
 		district: 7,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.999955, 40.651483],
+		zoom: 13.5
 	},
 	308: {
-		title: '',
+		title: 'Crown Heights',
 		neighborhoods: 'Crown Heights, Prospect Heights, and Weeksville',
 		borough: 'Brooklyn',
 		district: 8,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.944842, 40.673821],
+		zoom: 14
 	},
 	309: {
-		title: '',
+		title: 'Crown Heights',
 		neighborhoods: 'Crown Heights, Prospect Lefferts Gardens, and Wingate',
 		borough: 'Brooklyn',
 		district: 9,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.945415, 40.662651],
+		zoom: 14
 	},
 	310: {
-		title: '',
+		title: 'Fort Hamilton',
 		neighborhoods: 'Bay Ridge, Dyker Heights, and Fort Hamilton',
 		borough: 'Brooklyn',
 		district: 10,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-74.021365, 40.621461],
+		zoom: 13.5
 	},
 	311: {
-		title: '',
+		title: 'Bath Beach',
 		neighborhoods: 'Bath Beach, Gravesend, Mapleton, and Bensonhurst',
 		borough: 'Brooklyn',
 		district: 11,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.995562, 40.603541],
+		zoom: 13.5
 	},
 	312: {
-		title: '',
+		title: 'Boro Park',
 		neighborhoods: 'Boro Park, Kensington, Ocean Parkway, and Midwood',
 		borough: 'Brooklyn',
 		district: 12,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.979576, 40.624998],
+		zoom: 13.5
 	},
 	313: {
-		title: '',
+		title: 'Coney Island',
 		neighborhoods: 'Coney Island, Brighton Beach, Bensonhurst, Gravesend, and Seagate',
 		borough: 'Brooklyn',
 		district: 13,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.978729, 40.580953],
+		zoom: 13.5
 	},
 	314: {
-		title: '',
+		title: 'Flatbush',
 		neighborhoods: 'Flatbush, Midwood, Kensington, and Ocean Parkway',
 		borough: 'Brooklyn',
 		district: 14,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.957360, 40.626626],
+		zoom: 13.5
 	},
 	315: {
-		title: '',
+		title: 'Sheepshead Bay',
 		neighborhoods: 'Sheepshead Bay, Manhattan Beach, Kings Bay, Gerritsen Beach, Kings Highway, East Gravesend, Madison, Homecrest, and Plum Beach',
 		borough: 'Brooklyn',
 		district: 15,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.946066, 40.592131],
+		zoom: 13.5
 	},
 	316: {
-		title: '',
+		title: 'Brownsville',
 		neighborhoods: 'Brownsville and Ocean Hill',
 		borough: 'Brooklyn',
 		district: 16,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.909102, 40.665217],
+		zoom: 14
 	},
 	317: {
-		title: '',
+		title: 'East Flatbush',
 		neighborhoods: 'East Flatbush, Remsen Village, Farragut, Rugby, Erasmus and Ditmas Village',
 		borough: 'Brooklyn',
 		district: 17,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.930918, 40.646214],
+		zoom: 13.5
 	},
 	318: {
-		title: '',
+		title: 'Bergen Beach',
 		neighborhoods: 'Canarsie, Bergen Beach, Mill Basin, Flatlands, Marine Park, Georgetown, and Mill Island',
 		borough: 'Brooklyn',
 		district: 18,
-		center: [-73.986211, 40.720094],
+		center: [-73.913539, 40.616103],
 		zoom: 13
 	},
 	401: {
-		title: '',
+		title: 'Astoria',
 		neighborhoods: 'Astoria, Old Astoria, Long Island City, Queensbridge, Ditmars, Ravenswood, Steinway, Garden Bay, and Woodside',
 		borough: 'Queens',
 		district: 1,
-		center: [-74.008098, 40.711572],
+		center: [-73.917968, 40.767274],
 		zoom: 13
 	},
 	402: {
-		title: '',
+		title: 'Long Island City',
 		neighborhoods: 'Long Island City, Woodside, and Sunnyside',
 		borough: 'Queens',
 		district: 2,
-		center: [-73.930513, 40.737102],
+		center: [-73.923202, 40.737102],
 		zoom: 13
 	},
 	403: {
-		title: '',
+		title: 'Jackson Heights',
 		neighborhoods: 'Jackson Heights, East Elmhurst, North Corona, and La Guardia Airport',
 		borough: 'Queens',
 		district: 3,
-		center: [-73.986211, 40.720094],
+		center: [-73.873218, 40.757549],
 		zoom: 13
 	},
 	404: {
-		title: '',
+		title: 'Corona',
 		neighborhoods: 'Corona, Corona Heights, Elmhurst, and Newtown',
 		borough: 'Queens',
 		district: 4,
-		center: [-73.986211, 40.720094],
+		center: [-73.867514, 40.741435],
 		zoom: 13
 	},
 	405: {
-		title: '',
+		title: 'Maspeth',
 		neighborhoods: 'Ridgewood, Glendale, Middle Village, Maspeth, and Liberty Park',
 		borough: 'Queens',
 		district: 5,
-		center: [-73.986211, 40.720094],
+		center: [-73.886398, 40.706374],
 		zoom: 13
 	},
 	406: {
-		title: '',
+		title: 'Forest Hills',
 		neighborhoods: 'Forest Hills and Rego Park',
 		borough: 'Queens',
 		district: 6,
-		center: [-73.986211, 40.720094],
+		center: [-73.848477, 40.723140],
 		zoom: 13
 	},
 	407: {
-		title: '',
+		title: 'Flushing',
 		neighborhoods: 'Flushing, Bay Terrace, College Point, Whitestone, Malba, Beechhurst, Queensboro Hill, and Willets Point',
 		borough: 'Queens',
 		district: 7,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.812483, 40.762633],
+		zoom: 12.5
 	},
 	408: {
-		title: '',
+		title: 'Utopia',
 		neighborhoods: 'Fresh Meadows, Cunningham Heights, Hilltop Village, Pomonak Houses, Fresh Meadows, Jamaica Estates, Holliswood, Flushing South, Utopia, Kew Gardens Hills, and Briarwood',
 		borough: 'Queens',
 		district: 8,
-		center: [-73.986211, 40.720094],
+		center: [-73.793823, 40.723425],
 		zoom: 13
 	},
 	409: {
-		title: '',
+		title: 'Woodhaven',
 		neighborhoods: 'Richmond Hill, Woodhaven, Ozone Park, and Kew Gardens',
 		borough: 'Queens',
 		district: 9,
-		center: [-73.986211, 40.720094],
+		center: [-73.839423, 40.693657],
 		zoom: 13
 	},
 	410: {
-		title: '',
+		title: 'Howard Beach',
 		neighborhoods: 'Howard Beach, Ozone Park, South Ozone Park, Richmond Hill, Tudor Village, and Lindenwood',
 		borough: 'Queens',
 		district: 10,
-		center: [-73.986211, 40.720094],
+		center: [-73.832243, 40.665517],
 		zoom: 13
 	},
 	411: {
-		title: '',
+		title: 'Oakland Gardens',
 		neighborhoods: 'Bayside, Douglaston, Little Neck, Auburndale, East Flushing, Oakland Gardens, and Hollis Hills',
 		borough: 'Queens',
 		district: 11,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.757566, 40.749155],
+		zoom: 12.5
 	},
 	412: {
-		title: '',
+		title: 'Jamaica',
 		neighborhoods: 'Jamaica, Hollis, St. Albans, Springfield Gardens, Baisley Park, Rochdale Village, and South Jamaica',
 		borough: 'Queens',
 		district: 12,
-		center: [-73.986211, 40.720094],
+		center: [-73.777454, 40.686991],
 		zoom: 13
 	},
 	413: {
-		title: '',
+		title: 'Cambria Heights',
 		neighborhoods: 'Queens Village, Glen Oaks, New Hyde Park, Bellerose, Cambria Heights, Laurelton, Rosedale, Floral Park, and Brookville',
 		borough: 'Queens',
 		district: 13,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.738585, 40.691364],
+		zoom: 12
 	},
 	414: {
-		title: '',
+		title: 'Rockaway Beach',
 		neighborhoods: 'Breezy Point, Belle Harbor, Broad Channel, Neponsit, Arverne, Bayswater, Edgemere, Rockaway Park, Rockaway and Far Rockaway',
 		borough: 'Queens',
 		district: 14,
-		center: [-73.986211, 40.720094],
-		zoom: 13
+		center: [-73.833517, 40.580525],
+		zoom: 11.5
 	},
 	501: {
-		title: '',
+		title: 'North Staten Island',
 		neighborhoods: 'Arlington, Castleton Corners, Clifton, Concord, Elm Park, Fort Wadsworth, Graniteville, Grymes Hill, Livingston, Mariners Harbor, Meiers Corners, New Brighton, Port Ivory, Port Richmond, Randall Manor, Rosebank, St. George, Shore Acres, Silver Lake, Stapleton, Sunnyside, Tompkinsville, West Brighton, Westerleigh',
 		borough: 'Staten Island',
 		district: 1,
-		center: [-74.008098, 40.711572],
+		center: [-74.114690, 40.625387],
 		zoom: 12
 	},
 	502: {
-		title: '',
+		title: 'Central Staten Island',
 		neighborhoods: 'Arrochar, Bloomfield, Bulls Heads, Chelsea, Dongan Hills, Egbertville, Emerson Hill, Grant City, Grasmere, High Rock, Lighthouse Hill, Midland Beach, New Dorp, New Springville, Oakwood, Ocean Breeze, Old Town, Richmondtown, South Beach, Todt Hill, and Travis',
 		borough: 'Staten Island',
 		district: 2,
-		center: [-74.002776, 40.728160],
+		center: [-74.134523, 40.587088],
 		zoom: 12
 	},
 	503: {
-		title: '',
+		title: 'South Staten Island',
 		neighborhoods: 'Annadale, Arden Heights, Bay Terrace, Charleston, Eltingville, Great Kills, Greenridge, Huguenot, Pleasant Plains, Princes Bay, Richmondtown, Richmond Valley, Rossville, Tottenville, and Woodrow',
 		borough: 'Staten Island',
 		district: 3,
-		center: [-73.986211, 40.720094],
+		center: [-74.185964, 40.536227],
 		zoom: 12
 	}
 }
